660b7fb13599325c041ffb98672b9d90ae404288
samuelv2umc <11221103351@alunos.umc.br>
2026-03-04 12:39:18 -0300

    u

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -1,2 +1,2 @@
 export const WHATSAPP_LINK =
-  "https://wa.me/5511970768886?text=Olá!%20Gostaria%20de%20agendar%20uma%20consulta.";
+  "https://wa.me/5511970768886?text=Olá!%20Vim%20pelo%20site%20e%20gostaria%20de%20agendar%20uma%20consulta.";
